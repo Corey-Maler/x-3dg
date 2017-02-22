2dc54b3617cc102980cd4fa40129fc7524467104
Corey Maler <corey.maler@outlook.com>
2017-02-22 23:07:23 +0200

    work with css transform

--- src/engine/cube.ts
+++ src/engine/cube.ts
@@ -1,47 +1,82 @@
 import * as THREE from 'three';
 
-export const X3dgCubeProto = Object.create(HTMLElement.prototype);
-
-
-function parseColor(str: string):number {
+function parseColor(str: string): number {
     const reg = /rgb\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)/;
     const [_, r, g, b] = str.match(reg);
     return (parseInt(r, 0) << 8 << 8) + (parseInt(g, 0) << 8) + parseInt(b, 0);
 }
 
-X3dgCubeProto.attachedCallback = function() {
-    console.log('cube attached', this, this.parentElement);
-    const parent = this.parentElement;
-    const style = window.getComputedStyle(this);
-    const color = parseColor(style.color) || 0x000000;
-    console.log(color);
-    const rotationX = parseInt(this.getAttribute('rotationX')) || 0;
-    const material = new THREE.MeshBasicMaterial({
-        color, shading: THREE.FlatShading, wireframe: true, transparent: true
-    });
-    this.material = material;
-    const geometry = new THREE.CubeGeometry(1, 1, 1);
-    this.mesh = new THREE.Mesh(geometry, material);
+const reg2d = /matrix\((.*)\)/;
 
-    parent.add(this.mesh);
-    // window.getComputedStyle(cube).getPropertyValue('--custom')
+// "matrix3d(1, 0, 0, 0, 0, 0.866025, 0.5, 0, 0, -0.5, 0.866025, 0, 0, 0, 0, 1)"
+function parseMatrinx(matrix: string) {
+    const reg = /matrix3d\((.*)\)/;
+    let _, args;
+    if (reg.test(matrix)) {
+        // 2d matix
+        [_, args] = matrix.match(reg);
+        console.log('3d matrix >>', args);
+        return args.split(',').map(num => parseFloat(num));
+    } else {
+        [_, args] = matrix.match(reg2d);
+        const [a, b, c, d, e, f] = args.split(',').map(num => parseFloat(num));
+        const s = a === d ? a : 1;
+        console.log('2d matrix', args);
+        return [
+            a, c, e, 0,
+            b, d, f, 0,
+            0, 0, s, 0,
+            0, 0, 0, 1
+        ]
 
-    const self = this;
-
-    const redraw = () => {
-        const style = window.getComputedStyle(self);
-        self.material.color.setHex(parseColor(style.color));
-        //console.log(style.color);
-        window.requestAnimationFrame(redraw);
-    }
-
-    redraw();
-}
-
-X3dgCubeProto.attributeChangedCallback = function(attrName: string, oldValue: string, newValue: string) {
-    if (attrName === 'rotationy') {
-        this.mesh.rotation.y = parseFloat(newValue);
     }
 }
 
-export const X3dgCube = document.registerElement('x-3dg-cube', {prototype: X3dgCubeProto});
+export class X3dgCubeClass extends HTMLElement {
+    attachedCallback() {
+        const parent = this.parentElement;
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x000000, shading: THREE.FlatShading, wireframe: true, transparent: true
+        });
+        this.material = material;
+        const geometry = new THREE.CubeGeometry(1, 1, 1);
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.matrixAutoUpdate = false;
+        this.geometry = geometry;
+
+        this.computeStyle();
+
+        parent.add(this.mesh);
+        // window.getComputedStyle(cube).getPropertyValue('--custom')
+
+        const self = this;
+
+        const redraw = () => {
+            this.computeStyle();
+            //window.requestAnimationFrame(redraw);
+        }
+
+        redraw();
+
+    }
+
+    computeStyle() {
+        const style = window.getComputedStyle(this);
+        const color = parseColor(style.color);
+        const transform = style.transform;
+        //console.log('transform', transform);
+        if (color) this.material.color.setHex(color);
+        if (transform) {
+            const a = parseMatrinx(transform);
+            console.log('transform', transform, a);
+            this.mesh.matrix.fromArray(a);
+        }
+    }
+
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        console.log('atr changed >', attrName, oldValue, newValue);
+    }
+}
+
+export const X3dgCube = document.registerElement('x-3dg-cube', X3dgCubeClass);
